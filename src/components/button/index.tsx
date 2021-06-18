920e4a913aee1b3abf684c9704d45b3ad61aed8d
Jack Hermanson <jack.hermanson@live.com>
2021-06-18 10:38:45 -0600

    get lib to build

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,30 +2,15 @@ import * as React from "react";
 import { Button as ReactButton } from "reactstrap";
 
 export type Props = {
-    children: React.ReactNode;
     onClick?: () => void;
-    color?: string;
-    type?: string;
-    disabled?: boolean;
 };
 
-const Button: React.FC<Props> = ({
-    children,
-    onClick = () => {},
-    disabled = false,
-    type = "submit",
-}) => {
-    const handleClick = () => {
-        if (!disabled && onClick) onClick();
-    };
-
-    const rootProps = {
-        type,
-        onClick: handleClick,
-        disabled,
-    };
-
-    return <ReactButton color="primary">test</ReactButton>;
+const Button: React.FC<Props> = ({ onClick }) => {
+    return (
+        <ReactButton onClick={onClick} color="primary">
+            test
+        </ReactButton>
+    );
 };
 
 export default Button;
