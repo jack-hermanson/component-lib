b3c83c4d3fee35b97a3bbcf0870e6b7fbc50016b
Jack Hermanson <jack.hermanson@live.com>
2022-03-10 15:06:21 -0700

    add action card header link

--- src/components/ActionCardHeader/ActionCardHeader.tsx
+++ src/components/ActionCardHeader/ActionCardHeader.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import { CardHeader } from "reactstrap";
 
 interface Props {
     title: string;
     children?: React.ReactNode;
+    linkTo?: string;
 }
 
 export const ActionCardHeader: React.FC<Props> = ({
     title,
     children,
+    linkTo,
 }: Props) => {
     return (
         <CardHeader className="d-flex">
-            <h5 className="my-auto">{title}</h5>
+            <h5 className="my-auto">
+                {linkTo ? (
+                    <Link className="header-link" to={linkTo}>
+                        {title}
+                    </Link>
+                ) : (
+                    <Fragment>{title}</Fragment>
+                )}
+            </h5>
             {children && <div className="ms-auto">{children}</div>}
         </CardHeader>
     );
